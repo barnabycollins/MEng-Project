d76d1e949d722086c480a7c50dd100394f0e41b0
Barnaby Collins <barnabyc@sky.com>
2022-01-16 16:32:33 +0000

    Keep trying to tweak

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,19 @@ app.innerHTML = `
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
 
-import { Faust, FaustAudioWorkletNode, FaustScriptProcessorNode } from "faust2webaudio";
+import {Faust, FaustAudioWorkletNode, FaustScriptProcessorNode} from "faust2webaudio";
 const audioCtx = new window.AudioContext();
 const code = `
 import("stdfaust.lib");
 process = ba.pulsen(1, 10000) : pm.djembe(60, 0.3, 0.4, 1) <: dm.freeverb_demo;`;
-const polycode = `
+/*const polycode = `
 import("stdfaust.lib");
 process = ba.pulsen(1, 10000) : pm.djembe(ba.hz2midikey(freq), 0.3, 0.4, 1) * gate * gain with {
     freq = hslider("freq", 440, 40, 8000, 1);
     gain = hslider("gain", 0.5, 0, 1, 0.01);
     gate = button("gate");
 };
-effect = dm.freeverb_demo;`;
+effect = dm.freeverb_demo;`;*/
 
 const faust = new Faust({
   debug: true,
@@ -30,7 +30,7 @@ const faust = new Faust({
 await faust.ready;
 
 let node: FaustScriptProcessorNode | FaustAudioWorkletNode;
-node = faust.getNode(code, { audioCtx, useWorklet: window.AudioWorklet ? true : false, args: { "-I": "https://faust.grame.fr/tools/editor/libraries/" } })
+node = await faust.getNode(code, { audioCtx, useWorklet: window.AudioWorklet ? true : false, args: { "-I": "https://raw.githubusercontent.com/grame-cncm/faustlibraries/4cd48b91f1170498c1cf5d8ee5b87cda6cd797df/" } })
 .then(node => node.connect(audioCtx.destination));
 /*faust.getNode(polycode, { audioCtx, useWorklet: window.AudioWorklet ? true : false, voices: 4, args: { "-I": "https://faust.grame.fr/tools/editor/libraries/" } })
 .then(node => node.connect(audioCtx.destination));*/
