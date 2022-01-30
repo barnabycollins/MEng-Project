9fe39ee6e8dc4c4be9b4da1c4e565bbd2a9b6640
Barnaby Collins <barnabyc@sky.com>
2022-01-30 14:08:54 +0000

    Get UI messages working!!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {Faust, FaustAudioWorkletNode, FaustScriptProcessorNode} from "faust2webaudio";
 
+
 const audioCtx = new window.AudioContext();
 const code = `
 import("stdfaust.lib");
@@ -7,9 +8,9 @@ process = ba.pulsen(1, 10000) : pm.djembe(60, 0.3, 0.4, 1) <: dm.freeverb_demo;`
 const polycode = `
 import("stdfaust.lib");
 process = ba.pulsen(1, 10000) : pm.djembe(ba.hz2midikey(freq), 0.3, 0.4, 1) * gate * gain with {
-    freq = hslider("freq", 440, 40, 8000, 1);
-    gain = hslider("gain", 0.5, 0, 1, 0.01);
-    gate = button("gate");
+  freq = hslider("freq", 440, 40, 8000, 1);
+  gain = hslider("gain", 0.5, 0, 1, 0.01);
+  gate = button("gate");
 };
 effect = dm.freeverb_demo;`;
 
@@ -27,12 +28,11 @@ let node: FaustAudioWorkletNode;
 node = await faust.getNode(polycode, { audioCtx, useWorklet: true, voices: 4, args: { "-I": "libraries/" } }) as FaustAudioWorkletNode;
 console.log(node.getParams());
 
-// typescript-eslint-ignore
 node.connect(audioCtx.destination);
 node.keyOn(0, 60, 100);
 setTimeout(() => node.keyOn(0, 64, 40), 500);
 setTimeout(() => node.keyOn(0, 67, 80), 1000);
-setTimeout(() => node.allNotesOff(), 5000);
+//setTimeout(() => node.allNotesOff(), 5000);
 
 const uiWindow = (document.getElementById("ui-frame") as HTMLIFrameElement).contentWindow;
 node.setOutputParamHandler((path: string, value: number) => {
@@ -42,3 +42,7 @@ node.setOutputParamHandler((path: string, value: number) => {
 
 const msg = {type: "ui", ui: node.getUI()};
 uiWindow?.postMessage(msg, "*");
+
+window.addEventListener("message", (e) => {
+  node.setParamValue(e.data.path, e.data.value);
+});
