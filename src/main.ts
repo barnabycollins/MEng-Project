151f55a6f154839b8ff131917284225c67644382
Barnaby Collins <barnabyc@sky.com>
2022-01-30 18:25:23 +0000

    Tidy up

--- src/main.ts
+++ src/main.ts
@@ -2,38 +2,67 @@ import {Faust, FaustAudioWorkletNode} from "faust2webaudio";
 import {WebMidi} from "webmidi";
 import {code, polycode, midicode} from "./programs";
 
-const LOG = true;
 
+
+// LOGGING
+const LOG = false;
+function log(toLog: any): void {
+  /**
+   * Log a message if logging is enabled; otherwise do nothing
+   */
+  if (LOG) console.log("(DEBUG)", toLog);
+}
+
+
+
+// SOUND OUTPUT
+
+// Connect to Web Audio
 const audioCtx = new window.AudioContext();
 
+// Enable Faust compiler; wait until it's ready
 const faust = new Faust({
   debug: LOG,
   wasmLocation: "src/libfaust/libfaust-wasm.wasm",
   dataLocation: "src/libfaust/libfaust-wasm.data"
 });
-
 await faust.ready;
 
+// Compile a new Web Audio node from faust code
 let node: FaustAudioWorkletNode;
 node = await faust.getNode(midicode, { audioCtx, useWorklet: true, voices: 4, args: { "-I": "libraries/" } }) as FaustAudioWorkletNode;
 
+// Connect the node's output to Web Audio
 node.connect(audioCtx.destination);
 
+
+
+// UI CONTROLS
+
+// Find UI iframe
 const uiWindow = (document.getElementById("ui-frame") as HTMLIFrameElement).contentWindow;
+
+// Make the UI update when parameters change on the backend (eg, when a control is modulated by MIDI)
 node.setOutputParamHandler((path: string, value: number) => {
   const msg = {path, value, type: "param"};
   uiWindow?.postMessage(msg, "*");
 });
 
+// Send node parameters to the UI for display
 const msg = {type: "ui", ui: node.getUI()};
 uiWindow?.postMessage(msg, "*");
 
+// Add a listener for when we receive control changes from the iframe
 window.addEventListener("message", (e) => {
   node.setParamValue(e.data.path, e.data.value);
 });
 
-await WebMidi.enable();
 
+
+// MIDI SETUP
+
+// Set up MIDI
+await WebMidi.enable();
 if (WebMidi.inputs.length < 1) {
   console.log("No MIDI input devices detected.");
 }
@@ -41,7 +70,8 @@ else {
   WebMidi.inputs.forEach(device => {
     device.addListener("midimessage", e => {
       node.midiMessage(e.message.data);
-      if (LOG) console.log(`Received ${e.message.type} from ${device.name}`);
+
+      log(`Received ${e.message.type} from ${device.name}`);
     });
   });
 }
