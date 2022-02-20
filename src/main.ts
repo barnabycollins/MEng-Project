c02589cecfca712b1e660e4ab294afae5a1ac0ba
Barnaby Collins <barnabyc@sky.com>
2022-02-20 18:51:53 +0000

    Structure objects more sensibly

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import {WebMidi, Input, MessageEvent} from "webmidi";
 import {code, polycode, midicode} from "./programs";
 import * as c from "./constructs";
 
-const constructedCode = new c.AudioOutput([new c.MathsNode('*', new c.Oscillator('triangle', new c.MIDIFreq()), new c.Envelope(), new c.MIDIGain())]).getOutputString();
+const constructedCode = new c.AudioOutput([
+  new c.MathsNode('*', 
+    new c.Oscillator('triangle', new c.MIDIFreq()),
+    new c.Envelope(),
+    new c.MIDIGain()
+  )
+]).getOutputString();
 
 console.log(constructedCode);
 
